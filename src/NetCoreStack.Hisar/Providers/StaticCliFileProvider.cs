5ca823c8a742b2433d4d82438d9622c7d4bfd553
Gencebay <gencebay.demir@gmail.com>
2017-04-18 20:42:00 +0300

    Static Cli file provider, InMemoryFileInfo for assets

--- src/NetCoreStack.Hisar/Providers/StaticCliFileProvider.cs
+++ src/NetCoreStack.Hisar/Providers/StaticCliFileProvider.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
+using System.Text;
 
 namespace NetCoreStack.Hisar
 {
     public class StaticCliFileProvider : IFileProvider
     {
+        private static readonly HttpClient _client = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:1444")
+        };
+
         public StaticCliFileProvider()
         {
         }
@@ -18,8 +26,15 @@ namespace NetCoreStack.Hisar
 
         public IFileInfo GetFileInfo(string subpath)
         {
-            var name = Path.GetFileName(subpath);
-            return new NotFoundFileInfo(name);
+            var fullname = subpath;
+            if (fullname.StartsWith("/"))
+            {
+                fullname = fullname.Substring(1);
+            }
+
+            var name = Path.GetFileName(fullname);
+            var response = _client.GetStringAsync("getfile?fullname=" + fullname).GetAwaiter().GetResult();
+            return new InMemoryFileInfo(name, subpath, Encoding.UTF8.GetBytes(response), DateTime.UtcNow);
         }
 
         public IChangeToken Watch(string filter)
