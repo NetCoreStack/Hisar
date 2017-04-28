2f6cea4ad423051b3216f762cd0e535cc9712d84
Gencebay <gencebay.demir@gmail.com>
2017-04-28 12:52:48 +0300

    new componenthelper

--- test/components/Hisar.Component.Guideline/Startup.Component.cs
+++ test/components/Hisar.Component.Guideline/Startup.Component.cs
@@ -1,7 +1,6 @@
 // Hisar Cli auto generated component class!
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.ViewComponents;
 using NetCoreStack.Hisar;
 using System.Reflection;
 
@@ -21,33 +20,17 @@ namespace Hisar.Component.Guideline
 
         public static string ComponentContent(this IUrlHelper urlHelper, string contentPath)
         {
-            if (ComponentHelperBase.IsExternalComponent(urlHelper.ActionContext))
-            {
-                return urlHelper.Content(contentPath);
-            }
-
             return ComponentHelperBase.ResolveContentPath(urlHelper, ComponentId, contentPath);
         }
         
         public static string ResolveName(this ViewContext context, string name)
         {
-            if (ComponentHelperBase.IsExternalComponent(context))
-            {
-                return name;
-            }
-
-            return $"{_assemblyName}.{name}";
+            return ComponentHelperBase.ResolveViewComponentName(context, _assemblyName, name);
         }
 
         public static string ResolveName<TComponent>(this ViewContext context)
         {
-            if (ComponentHelperBase.IsExternalComponent(context))
-            {
-                return ViewComponentConventions.GetComponentName(typeof(TComponent).GetTypeInfo());
-            }
-
-            var componentName = ViewComponentConventions.GetComponentName(typeof(TComponent).GetTypeInfo());
-            return $"{_assemblyName}.{componentName}";
+            return ComponentHelperBase.ResolveViewComponentName<TComponent>(context, _assemblyName);
         }
     }
 }
