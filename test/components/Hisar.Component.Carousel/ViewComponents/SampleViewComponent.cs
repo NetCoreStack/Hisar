7d9d7f1566782b9859b629ed528f8512f6cb0630
Gencebay <gencebay.demir@gmail.com>
2017-05-03 10:12:31 +0300

    ViewComponent invoke hosting side samples

---
+++ test/components/Hisar.Component.Carousel/ViewComponents/SampleViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreStack.Hisar;
+using System;
+using System.Threading.Tasks;
+
+namespace Hisar.Component.Carousel.ViewComponents
+{
+    public class SampleViewComponent : HisarViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            await Task.CompletedTask;
+            return View(new CarouselViewModel { Title = ExecutionComponentId + " Component -> Sample View Component: " + DateTime.Now });
+        }
+    }
+}
