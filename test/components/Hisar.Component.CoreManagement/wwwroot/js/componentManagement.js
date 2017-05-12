664c54f05509ea0f57858ee317fd84e5f1f8130d
Taha İPEK <ti.tahaipek@gmail.com>
2017-05-12 20:00:49 +0300

    Component management js updated

--- test/components/Hisar.Component.CoreManagement/wwwroot/js/componentManagement.js
+++ test/components/Hisar.Component.CoreManagement/wwwroot/js/componentManagement.js
@@ -16,66 +16,6 @@
         }
     };
 
-    var initialData = [
-        {
-            packageId: 'CarouselApp',
-            packageVersion: '1.2.3.4',
-            authors: 'Gencebay Demir',
-            company: 'Bilge Adam Bilişim Hiz. Ltd. Şti.',
-            product: 'Carousel',
-            description: 'NetcoreStack Hisar Carousel',
-            copyright: 'Opengl Licence',
-            licenceUrl: 'http://opengl.com',
-            projectUrl: 'https://github.com/NetCoreStack/Hisar',
-            iconUrl: 'https://cdn0.iconfinder.com/data/icons/summer-2/128/Summer_512px-04.png',
-            repositoryUrl: 'https://github.com/NetCoreStack/Hisar',
-            tags: 'Hisar,Carousel,Slider,Plugin',
-            releaseNotes: '<section><h2>Your placeholder text :</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ad rem redeamus; Sumenda potius quam expetenda. <mark>Maximus dolor, inquit, brevis est.</mark> Duo Reges: constructio interrete.</p><ol><li>Atque haec ita iustitiae propria sunt, ut sint virtutum reliquarum communia.</li><li>Hoc simile tandem est?</li><li>Ad eas enim res ab Epicuro praecepta dantur.</li><li>Ut in voluptate sit, qui epuletur, in dolore, qui torqueatur.</li></ol><pre> 	 Quae quod Aristoni et Pyrrhoni omnino visa sunt pro nihilo, 	 ut inter optime valere et gravissime aegrotare nihil prorsus 	 dicerent interesse, recte iam pridem contra eos desitum est 	 disputari. 	 Contineo me ab exemplis. 	 </pre></section>',
-            neutrelLanguage: 'Tuskish,English',
-            version: '2.0.0.0',
-            fileVersion: '3.0.0.0',
-            components: [{ name: 'Carousel' }, { name: 'Sample' }]
-        },
-        {
-            packageId: 'ComponentManagementApp',
-            packageVersion: '1.2.3.4',
-            authors: 'Taha İPEK',
-            company: 'Bilge Adam Bilişim Hiz. Ltd. Şti.',
-            product: 'Component Management',
-            description: 'NetcoreStack Hisar Component Management',
-            copyright: 'Opengl Licence',
-            licenceUrl: 'http://opengl.com',
-            projectUrl: 'https://github.com/NetCoreStack/Hisar',
-            iconUrl: 'https://cdn0.iconfinder.com/data/icons/summer-2/128/Summer_512px-04.png',
-            repositoryUrl: 'https://github.com/NetCoreStack/Hisar',
-            tags: 'Hisar,Component,Management,Plugin',
-            releaseNotes: '<section><h2>Your placeholder text :</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ad rem redeamus; Sumenda potius quam expetenda. <mark>Maximus dolor, inquit, brevis est.</mark> Duo Reges: constructio interrete.</p><ol><li>Atque haec ita iustitiae propria sunt, ut sint virtutum reliquarum communia.</li><li>Hoc simile tandem est?</li><li>Ad eas enim res ab Epicuro praecepta dantur.</li><li>Ut in voluptate sit, qui epuletur, in dolore, qui torqueatur.</li></ol><pre> 	 Quae quod Aristoni et Pyrrhoni omnino visa sunt pro nihilo, 	 ut inter optime valere et gravissime aegrotare nihil prorsus 	 dicerent interesse, recte iam pridem contra eos desitum est 	 disputari. 	 Contineo me ab exemplis. 	 </pre></section>',
-            neutrelLanguage: 'Tuskish,English',
-            version: '2.0.0.0',
-            fileVersion: '3.0.0.0',
-            components: [{ name: 'Component' }]
-        },
-        {
-            packageId: 'GuidelineApp',
-            packageVersion: '1.2.3.4',
-            authors: 'Gencebay Demir',
-            company: 'Bilge Adam Bilişim Hiz. Ltd. Şti.',
-            product: 'Carousel',
-            description: 'NetcoreStack Hisar Guideline',
-            copyright: 'Opengl Licence',
-            licenceUrl: 'http://opengl.com',
-            projectUrl: 'https://github.com/NetCoreStack/Hisar',
-            iconUrl: 'https://cdn0.iconfinder.com/data/icons/tutor-icon-set/512/set_of_three_books-128.png',
-            repositoryUrl: 'https://github.com/NetCoreStack/Hisar',
-            tags: 'Hisar,Guideline,Plugin',
-            releaseNotes: '<section><h2>Your placeholder text :</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ad rem redeamus; Sumenda potius quam expetenda. <mark>Maximus dolor, inquit, brevis est.</mark> Duo Reges: constructio interrete.</p><ol><li>Atque haec ita iustitiae propria sunt, ut sint virtutum reliquarum communia.</li><li>Hoc simile tandem est?</li><li>Ad eas enim res ab Epicuro praecepta dantur.</li><li>Ut in voluptate sit, qui epuletur, in dolore, qui torqueatur.</li></ol><pre> 	 Quae quod Aristoni et Pyrrhoni omnino visa sunt pro nihilo, 	 ut inter optime valere et gravissime aegrotare nihil prorsus 	 dicerent interesse, recte iam pridem contra eos desitum est 	 disputari. 	 Contineo me ab exemplis. 	 </pre></section>',
-            neutrelLanguage: 'Tuskish,English',
-            version: '2.0.0.0',
-            fileVersion: '3.0.0.0',
-            components: [{ name: 'Guideline' }]
-        },
-    ];
-
     var CmAssemblyModel = function (item) {
         var self = this;
         self.Id = ko.bindingHandlers.uniqueId;
@@ -99,12 +39,17 @@
         self.Components = item.components;
     };
 
-    var CmComponentViewModel = function (datas) {
+    var CmComponentViewModel = function () {
 
         var self = this;
         self.assemblies = ko.observableArray([]);
+        self.selectedAssemblyDetails = ko.observable();
 
-        self.selectedAssemblyItem = ko.observable();
+        self.selectedAssemblyItem = function (value) {
+            console.log(value);
+            self.selectedAssemblyDetails(value);
+            $('input:checkbox[data-toggle="toggle"]').bootstrapToggle();
+        };
 
         self.fetchAssemblies = function () {
             console.log("fetched");
@@ -115,8 +60,11 @@
                 contentType: 'application/json; charset=utf-8',
                 success: function (data, textStatus, jqXHR) {
                     self.assemblies(ko.utils.arrayMap(data, function (c) {
-                        return new CmAssemblyModel(c);
+                        var model = new CmAssemblyModel(c);
+                        self.selectedAssemblyItem(model);
+                        return model;
                     }));
+                   
                     console.log("success");
                 },
                 error: function (response) {
@@ -135,7 +83,7 @@
 
     };
 
-    ko.applyBindings(new CmComponentViewModel(initialData), document.getElementById("cmContainer"));
+    ko.applyBindings(new CmComponentViewModel(), document.getElementById("cmContainer"));
 
 
 })($, window, ko, undefined);
