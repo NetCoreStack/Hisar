d301a33910a3242adcddd478b223444a36c04b9f
Gencebay Demir <gencebay.demir@gmail.com>
2017-05-22 10:45:01 +0300

    entity identity active type

---
+++ src/NetCoreStack.Hisar/Types/EntityIdentityActiveBson.cs
@@ -0,0 +1,9 @@
+﻿using NetCoreStack.Data.Contracts;
+
+namespace NetCoreStack.Hisar
+{
+    public class EntityIdentityActiveBson : EntityIdentityBson
+    {
+        public bool Active { get; set; }
+    }
+}
