7de3a09541978906f9b7d18649309ba5d3cc43a8
Gencebay Demir <gencebay.demir@gmail.com>
2017-05-30 15:43:35 +0300

    auto complete request extension

---
+++ src/NetCoreStack.Hisar/Extensions/AutoCompleteRequestExtensions.cs
@@ -0,0 +1,39 @@
+﻿using NetCoreStack.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetCoreStack.Hisar
+{
+    public static class AutoCompleteRequestExtensions
+    {
+        public static bool HasSelectedIds(this AutoCompleteRequest request)
+        {
+            if (request == null)
+            {
+                return false;
+            }
+
+            if (request.SelectedIds == null)
+            {
+                return false;
+            }
+
+            if (request.SelectedIds.Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static IEnumerable<string> GetSelectedIds(this AutoCompleteRequest request)
+        {
+            if (request.HasSelectedIds())
+            {
+                return request.SelectedIds;
+            }
+
+            return null;
+        }
+    }
+}
